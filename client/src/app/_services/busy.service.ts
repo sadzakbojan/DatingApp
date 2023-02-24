371fde6784f593ae4f6b98da6474c3ada77b6b0d
sadzakbojan <62440200+sadzakbojan@users.noreply.github.com>
2023-02-24 18:49:13 +0100

    End of section 10

---
+++ client/src/app/_services/busy.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BusyService {
+  busyRequestCount = 0;
+
+  constructor(private spinnerService: NgxSpinnerService) { }
+
+  busy(){
+    this.busyRequestCount++;
+    this.spinnerService.show(undefined, {
+      size: "large",
+      bdColor: "rgba(0, 0, 0, 1)",
+      color: "#333333",
+      template:"<img src='./assets/loader.gif' />",
+    })
+  }
+
+  idle(){
+    this.busyRequestCount--;
+    if(this.busyRequestCount <=0){
+      this.busyRequestCount = 0;
+      this.spinnerService.hide();
+    }
+  }
+}
